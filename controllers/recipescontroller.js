699deec651e11c446adfa231b05228f97f55f730
Chentao Tang <chentao.tang@mail.utoronto.ca>
2019-06-27 19:11:46 -0400

    refactored controllers to require less information

--- controllers/recipescontroller.js
+++ controllers/recipescontroller.js
@@ -1,34 +1,34 @@
-const db = require("../models");
+const Recipe = require("../models/recipe");
 
-// Defining methods for the recipeController
+// Defining methods for the recipesController
 module.exports = {
   findAll: function(req, res) {
-    db.Recipe
+    Recipe
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Recipe
+    Recipe
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Recipe
+    Recipe
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Recipe
+    Recipe
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Recipe
+    Recipe
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
