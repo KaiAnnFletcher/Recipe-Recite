2a788cdf2144a9691b82dc760f9419aeca8be483
Chentao Tang <chentao.tang@mail.utoronto.ca>
2019-07-09 15:46:55 -0400

    setup encryption and basic route to accept and create a user

--- models/user.js
+++ models/user.js
@@ -1,11 +1,44 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
 
 const userSchema = new Schema({
   username: {type: String, required: true, unique: true},
   password: {type: String, required: true},
   favorites: []
 });
+
+userSchema.pre('save', function(next) {
+  // Check if document is new or a new password has been set
+  if (this.isNew || this.isModified('password')) {
+    // Saving reference to this because of changing scopes
+    const document = this;
+    bcrypt.hash(document.password, saltRounds,
+      function(err, hashedPassword) {
+      if (err) {
+        next(err);
+      }
+      else {
+        document.password = hashedPassword;
+        next();
+      }
+    });
+  } else {
+    next();
+  }
+})
+
+userSchema.methods.isCorrectPassword = function(password, callback){
+  bcrypt.compare(password, this.password, function(err, same) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(err, same);
+    }
+  });
+}
 
 const User = mongoose.model("users", userSchema);
 
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,5 +1,20 @@
 const router = require("express").Router();
 const usersController = require("../../controllers/userscontroller");
+const User = require('../../models/user.js');
+
+// POST route to register a user
+router.post('/api/register', function(req, res) {
+  const { email, password } = req.body;
+  const user = new User({ email, password });
+  user.save(function(err) {
+    if (err) {
+      res.status(500)
+        .send("Error registering new user please try again.");
+    } else {
+      res.status(200).send("Welcome to the club!");
+    }
+  });
+});
 
 router.route("/")
   .get(usersController.findOne)
